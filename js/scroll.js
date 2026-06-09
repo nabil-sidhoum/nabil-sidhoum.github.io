b5c6373b52a5d8aca1692783221a49b4d66ebc2a
nabil-sidhoum <nabil-sidhoum@users.noreply.github.com>
2026-06-09 13:05:30 +0000

    deploy: f3b9ef2788f3756125c1411400739e7012c510bf

--- js/scroll.js
+++ js/scroll.js
@@ -1,28 +1,7 @@
-﻿window.scrollHelper = {
-    init: function () {
-        const mybutton = document.getElementById("topbtn");
-        const mainContent = document.querySelector(".main-content");
+export function lockScroll() {
+    document.body.style.overflow = 'hidden';
+}
 
-        if (!mybutton || !mainContent) {
-            console.error("❌ Élément introuvable !");
-            return;
-        }
-
-        // On cache le bouton au départ
-        mybutton.style.display = "none";
-
-        // ✅ Ici on écoute le scroll de main-content (pas window)
-        mainContent.addEventListener("scroll", () => {
-            if (mainContent.scrollTop > 20) {
-                mybutton.style.display = "grid";
-            } else {
-                mybutton.style.display = "none";
-            }
-        });
-
-        // Scroll vers le haut en douceur
-        mybutton.addEventListener("click", () => {
-            mainContent.scrollTo({ top: 0, behavior: "smooth" });
-        });
-    }
-};
+export function unlockScroll() {
+    document.body.style.removeProperty('overflow');
+}
